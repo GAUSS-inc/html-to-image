5e1cbb715b0953724d4258671aab0f116518a3d8
sho <sho.yamamoto@gauss-ai.jp>
2021-11-10 18:06:47 +0900

    fix: chrome対応

--- src/util.ts
+++ src/util.ts
@@ -142,6 +142,12 @@ export function createImage(url: string): Promise<HTMLImageElement> {
 export async function svgToDataURL(svg: SVGElement): Promise<string> {
   return Promise.resolve()
     .then(() => new XMLSerializer().serializeToString(svg))
+    .then((string) =>
+      string.replace(
+        /background-image:/g,
+        '-webkit-background-clip: text; background-image:',
+      ),
+    )
     .then(encodeURIComponent)
     .then((html) => `data:image/svg+xml;charset=utf-8,${html}`)
 }
